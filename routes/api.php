82fa888d6003662758c2121d58e0d31334cbe077
Muhammad Imam Al Amin <imamalamin485@gmail.com>
2026-03-25 10:57:35 +0700

    feat: create Route endpoint for api

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware(['auth:sanctum'])->group(function () {
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+    
+    Route::middleware('permission:akses menu user management')->group(function () {
+        Route::get('/users', [UserController::class, 'index']);
+    });
+});
