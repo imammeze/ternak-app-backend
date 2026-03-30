405568c73ba9d7ec133769433ae3cf344a468e7e
Muhammad Imam Al Amin <imamalamin485@gmail.com>
2026-03-30 10:41:06 +0700

    refactor: update UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,7 +17,34 @@ class UserSeeder extends Seeder
                 'password' => Hash::make('admin1234'), 
             ]
         );
+
+        $manager = User::firstOrCreate(
+            ['email' => 'manager@peternakan.com'],
+            [
+                'name' => 'Manager',
+                'password' => Hash::make('manager1234'), 
+            ]
+        );
+
+        $stakeholder = User::firstOrCreate(
+            ['email' => 'stakeholder@peternakan.com'],
+            [
+                'name' => 'Stakeholder',
+                'password' => Hash::make('stakeholder1234'), 
+            ]
+        );
+
+         $karyawan = User::firstOrCreate(
+            ['email' => 'karyawan@peternakan.com'],
+            [
+                'name' => 'Karyawan',
+                'password' => Hash::make('karyawan1234'), 
+            ]
+        );
         
         $admin->assignRole('admin');
+        $manager->assignRole('manager');
+        $stakeholder->assignRole('stakeholder');
+        $karyawan->assignRole('karyawan');
     }
 }
