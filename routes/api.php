a6dae0715d98e7c1b5dbade10595a693f77c271e
Muhammad Imam Al Amin <imamalamin485@gmail.com>
2026-04-10 23:28:34 +0700

    chore: update route with prefix

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Api\AbsensiController;
 use App\Http\Controllers\Api\ActivityController;
+use App\Http\Controllers\Api\AktivitasController;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\KandangController;
 use App\Http\Controllers\Api\PengeluaranSusuController;
 use App\Http\Controllers\Api\PerawatanTernakController;
 use App\Http\Controllers\Api\PerpindahanTernakController;
@@ -18,31 +21,54 @@ Route::middleware(['auth:sanctum'])->group(function () {
 
     Route::post('/logout', [AuthController::class, 'logout']);
     
-    Route::middleware('permission:akses menu user management')->group(function () {
-        Route::get('/users', [UserController::class, 'index']);
-        Route::post('/users', [UserController::class, 'store']);
+    Route::get('/produksi-susu', [ProduksiSusuController::class, 'index']);
+    Route::get('/pengeluaran-susu', [PengeluaranSusuController::class, 'index']);
+    Route::get('/activity', [ActivityController::class, 'index']);
+    
+    Route::prefix('ternak')->controller(TernakController::class)->group(function () {
+        Route::get('/', 'index');
+        Route::get('/{id}', 'show');
+    });
+    
+    Route::prefix('kandang')->controller(KandangController::class)->group(function () {
+        Route::get('/', 'index');
+        Route::put('/{id}', 'update');
+        Route::delete('/{id}', 'destroy');
+    });
+    
+    Route::prefix('absen')->controller(AbsensiController::class)->group(function () {
+        Route::post('/masuk', 'presensiMasuk');
+        Route::post('/keluar', 'presensiKeluar');
+        Route::post('/mulai-lembur', 'mulaiLembur');
+        Route::post('/selesai-lembur', 'selesaiLembur');
+        Route::get('/histori', 'histori');
+    });
+    
+    Route::prefix('aktivitas')->controller(AktivitasController::class)->group(function () {
+        Route::get('/', 'index');
+        Route::post('/', 'store');
+        Route::put('/{id}', 'update');
+        Route::delete('/{id}', 'destroy');
+    });
+    
+    Route::get('/list-stakeholders', function () {
+        $stakeholders = User::role('stakeholder')->select('id', 'name')->get();
+        return response()->json($stakeholders, 200);
     });
 
     Route::middleware('permission:akses menu input data')->group(function () {
-        Route::get('/ternak', [TernakController::class, 'index']);
         Route::post('/ternak', [TernakController::class, 'store']);
-        Route::get('/ternak/{id}', [TernakController::class, 'show']);
-
-        Route::get('/produksi-susu', [ProduksiSusuController::class, 'index']);
         Route::post('/produksi-susu', [ProduksiSusuController::class, 'store']);
-
         Route::post('/perawatan-ternak', [PerawatanTernakController::class, 'store']);
-
-        Route::get('/pengeluaran-susu', [PengeluaranSusuController::class, 'index']);
         Route::post('/pengeluaran-susu', [PengeluaranSusuController::class, 'store']);
-
         Route::post('/perpindahan-ternak', [PerpindahanTernakController::class, 'store']);
-        
-        Route::get('/list-stakeholders', function () {
-            $stakeholders = User::role('stakeholder')->select('id', 'name')->get();
-            return response()->json($stakeholders, 200);
-        });
+        Route::post('/kandang', [KandangController::class, 'store']);
     });
 
-    Route::get('/activity', [ActivityController::class, 'index']);
+    Route::middleware('permission:akses menu user management')->group(function () {
+        Route::get('/users', [UserController::class, 'index']);
+        Route::post('/users', [UserController::class, 'store']);
+        Route::put('/users/{id}', [UserController::class, 'update']);
+    });
+
 });
